e58d485f2d5923b828e1c2c129df2d7af4b7ff16
Nataliya Hrynchyshyn <160016021+NataliyaHr@users.noreply.github.com>
2024-07-09 14:28:42 +0200

    commit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,16 +6,26 @@ const formData = {
     this.email = newemail;
     this.message = newmessage;
   },
+
+  saveToLocalStorage() {
+    localStorage.setItem(LSTORAGE_KEY, JSON.stringify(this));
+  },
+
+  clear() {
+    this.email = "";
+    this.message = "";
+  },
 };
 
 const formMessage = document.querySelector(".feedback-form");
 const LSTORAGE_KEY = "feedback-form-state";
+
 formMessage.addEventListener("input", inputText);
 formMessage.addEventListener("submit", onSubmit);
 
 function getValueOfLocalstorage() {
   const localdata = JSON.parse(localStorage.getItem(LSTORAGE_KEY));
-  if (localdata !== null) {
+  if (localdata) {
     formData.setFormValue(localdata.email, localdata.message);
     formMessage.email.value = formData.email;
     formMessage.message.value = formData.message;
@@ -23,26 +33,24 @@ function getValueOfLocalstorage() {
 }
 
 function inputText(event) {
-  const eventName = event.target;
-  switch (eventName.nodeName) {
-    case "INPUT":
-      formData.email = eventName.value.trim();
-      localStorage.setItem(LSTORAGE_KEY, JSON.stringify(formData));
-      break;
-    case "TEXTAREA":
-      formData.message = eventName.value.trim();
-      localStorage.setItem(LSTORAGE_KEY, JSON.stringify(formData));
-      break;
+  const { name, value } = event.target;
+  if (name === "email") {
+    formData.email = value.trim();
+  } else if (name === "message") {
+    formData.message = value.trim();
   }
+  formData.saveToLocalStorage();
 }
 
 function onSubmit(event) {
   event.preventDefault();
   if (formData.email === "" || formData.message === "") {
-    return alert("Fill please all fields");
+    return alert("Please fill in all fields");
   }
-  console.table(formData);
+  console.log(formData);
   localStorage.removeItem(LSTORAGE_KEY);
+  formData.clear();
   formMessage.reset();
+formData.saveToLocalStorage();
 }
 getValueOfLocalstorage();
